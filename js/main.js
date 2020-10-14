90cc37840d4e8bd39e230374d62875ae26f397bb
Josh Nussbaum <Joshnussbaum89@gmail.com>
2020-10-13 23:48:14 -0400

    start metronome UI

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
 /* ============================================= */
 /*              JavaScript                       */
 /* ============================================= */
+
+const setBPM = document.querySelector('#set-BPM');
+
+// listens for user input 
+// prints the tempo to the page when user clicks 'set tempo' button
+setBPM.addEventListener('keyup', (event) => {
+
+    const setTempoBtn = document.querySelector('.set-tempo-btn');
+    const printTempo = document.querySelector('.print-tempo');
+    const eventTargetVal = event.target.value;
+
+    if (!isNaN(eventTargetVal) && Number.isInteger(+eventTargetVal)) {
+        setTempoBtn.addEventListener('click', () => {
+
+            if (eventTargetVal) {
+                printTempo.innerHTML = `<h1>${eventTargetVal} BPM</h1>`;
+            } else {
+                printTempo.innerHTML = `<p>Please enter a number</p>`;
+            }
+
+        });
+    }
+
+});
